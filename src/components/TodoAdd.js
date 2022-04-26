a01abf6180662d3e66bdb503ddb315019a60f73a
Steve Smodish <smodish@hotmail.com>
2022-04-26 16:06:41 -0400

    redux todo app complete

---
+++ src/components/TodoAdd.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { addTodo } from '../redux/reducers/todo/actions'
+
+const TodoAdd = () => {
+  const dispatch = useDispatch()
+  const [todo, setTodo] = useState('')
+
+  const onAddButtonClick = () => {
+    if (!todo) return
+
+    dispatch(addTodo(todo))
+    setTodo('')
+  }
+
+  return (
+    <div>
+      <input type='text' placeholder='Start typing...' value={todo} onChange={(e) => setTodo(e.target.value)} />
+      <button onClick={onAddButtonClick}>Add Todo</button>
+    </div>
+  )
+}
+
+export default TodoAdd
