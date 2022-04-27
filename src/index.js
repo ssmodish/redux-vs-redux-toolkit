eda0800e9f770691ee374884322936d95313ca87
Steve Smodish <smodish@hotmail.com>
2022-04-27 09:47:33 -0400

    redux-toolkit version

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css'
 import TodoContainer from './components/TodoContainer'
 
 import { Provider } from 'react-redux'
-import store from './redux/store'
+import store from './redux-toolkit/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
